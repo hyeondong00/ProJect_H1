f551e5ded1f1f8e3baffb73097fceeceb2c89ed6
hyeondong00 <hydo1126@gmail.com>
2025-02-18 21:10:40 +0900

    미니게임 오브젝트 상호작용 완료

--- Assets/Script/MiniGameEntrance.cs
+++ Assets/Script/MiniGameEntrance.cs
@@ -1,16 +1,17 @@
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class MiniGameEntrance : MonoBehaviour
 {
     private bool isPlayerInRange = false;
 
     void Update()
+
     {
         // 플레이어가 미니게임 입구에 가까이 있을 때 'E' 키로 미니게임 시작
         if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            LoadMiniGameScene();
+            LoadMiniGameScenes();
         }
     }
 
@@ -30,9 +31,11 @@ public class MiniGameEntrance : MonoBehaviour
         }
     }
 
-    void LoadMiniGameScene()
+    void LoadMiniGameScenes()
     {
         // 미니게임 씬 로드
-        SceneManager.LoadScene("MiniGameScene"); // "MiniGameScene"을 미니게임 씬의 이름으로 변경
+        SceneManager.LoadScene("MiniGameScenes");
+
     }
+   
 }
